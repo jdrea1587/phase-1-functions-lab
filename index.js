95895d0f2975fc2f27c3df35ea1986d35fb8d7d8
Joshua <joshdanielrea@gmail.com>
2021-06-29 21:32:34 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+const headquarters = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks(distance) {
+    if (distance > 42) {
+        return distance - headquarters;
+    }
+    else if (distance < 42){
+        return headquarters -distance;
+    }
+}
+
+function distanceFromHqInFeet (distance) {
+    return distanceFromHqInBlocks(distance) * feet;
+}
+
+function distanceTravelledInFeet (headquarters, travel) {
+    if (travel > headquarters){
+        return (travel - headquarters) * feet;
+    }
+    else {
+        return (headquarters - travel) * feet;
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    const sumFeet = distanceTravelledInFeet(start, destination)
+    let cost = 0;
+    if (sumFeet < 400) {
+        return 0;
+    }
+    else if ((sumFeet >= 400) && (sumFeet <= 2000)) {
+        return cost = (sumFeet - 400) * .02;
+    }
+    else if ((sumFeet > 2000) && (sumFeet < 2500)) {
+        return 25;
+    }
+    else if (sumFeet > 2500) {
+        return 'cannot travel that far';
+    }
+}
